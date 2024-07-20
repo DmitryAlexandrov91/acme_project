d39a9b4f4c2cf5bc05954b1ed7d453a5fa94a1cc
Dmitry Alexandrov <alexandrov.dm@mail.ru>
2024-07-20 15:29:57 +0300

    acme_progect edits

--- acme_project/birthday/admin.py
+++ acme_project/birthday/admin.py
@@ -1,1 +1,18 @@
 from django.contrib import admin
+
+from .models import Birthday
+
+
+class BirthdayAdmin(admin.ModelAdmin):
+    list_display = (
+        'first_name',
+        'last_name',
+        'birthday',
+    )
+    list_editable = (
+        'last_name',
+        'birthday',
+    )
+
+
+admin.site.register(Birthday, BirthdayAdmin)
